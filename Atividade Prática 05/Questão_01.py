320b9c604c93e983005003b4d060bd0e47f051c3
Erick-Epifanio <erickepifaniodasilva@gmail.com>
2025-09-16 13:13:41 -0300

    Delete Atividade Prática 05/Questão_01.py

--- Atividade Prática 05/Questão_01.py
+++
@@ -1,9 +0,0 @@
-def calcular_gorjeta(valor_da_conta, porcentagem_gorjeta):
-    gorjeta = valor_da_conta * (porcentagem_gorjeta/100)
-    return gorjeta
-
-total_conta = int(input("digite o total da conta: "))
-porcentagem = int(input("Quantos porcentos de gorjeta deseja pagar?: "))
-
-gorjeta = calcular_gorjeta(total_conta, porcentagem)
-print(f"Para uma conta de R${total_conta}, você deseja pagar {porcentagem}% ---- R${gorjeta} de gorjeta")
